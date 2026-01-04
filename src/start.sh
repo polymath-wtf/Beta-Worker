cf5f7b4f0d1e93aa57b8030dc24268ed5b5d8a42
polymath-wtf <147452692+polymath-wtf@users.noreply.github.com>
2026-01-04 22:53:50 +0200

    Enable sage attention and GPU-only mode in start.sh

--- src/start.sh
+++ src/start.sh
@@ -14,12 +14,12 @@ echo "worker-comfyui: Starting ComfyUI"
 
 # Serve the API and don't shutdown the container
 if [ "$SERVE_API_LOCALLY" == "true" ]; then
-    python -u /comfyui/main.py --disable-auto-launch --highvram --disable-metadata --listen --verbose "${COMFY_LOG_LEVEL}" --log-stdout &
+    python -u /comfyui/main.py --disable-auto-launch --highvram --use-sage-attention --gpu-only --disable-metadata --listen --verbose "${COMFY_LOG_LEVEL}" --log-stdout &
 
     echo "worker-comfyui: Starting RunPod Handler"
     python -u /handler.py --rp_serve_api --rp_api_host=0.0.0.0
 else
-    python -u /comfyui/main.py --disable-auto-launch --highvram --disable-metadata --verbose "${COMFY_LOG_LEVEL}" --log-stdout &
+    python -u /comfyui/main.py --disable-auto-launch --highvram --use-sage-attention --gpu-only --disable-metadata --verbose "${COMFY_LOG_LEVEL}" --log-stdout &
 
     echo "worker-comfyui: Starting RunPod Handler"
     python -u /handler.py
