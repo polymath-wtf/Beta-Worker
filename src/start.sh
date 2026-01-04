8b6b04bbbfcd267e42fc31fad4c92ec122ebf056
polymath-wtf <147452692+polymath-wtf@users.noreply.github.com>
2026-01-05 00:04:56 +0200

    qwe

--- src/start.sh
+++ src/start.sh
@@ -14,7 +14,7 @@ echo "worker-comfyui: Starting ComfyUI"
 
 # Serve the API and don't shutdown the container
 if [ "$SERVE_API_LOCALLY" == "true" ]; then
-    python -u /comfyui/main.py --disable-auto-launch --highvram --use-sage-attention --disable-metadata --listen --verbose "${COMFY_LOG_LEVEL}" --log-stdout &
+    python -u /comfyui/main.py --disable-auto-launch --highvram --use-sage-attention --disable-metadata --listen 0.0.0.0 --verbose "${COMFY_LOG_LEVEL}" --log-stdout &
 
     echo "worker-comfyui: Starting RunPod Handler"
     python -u /handler.py --rp_serve_api --rp_api_host=0.0.0.0
